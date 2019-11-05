89e7371452b4fe700b2b0710f226f7b555f800db
Colin Fendrick <colin.fendrick@gmail.com>
2019-11-05 12:33:24 -0500

    :sparkles: Add in axios data fetching and loading

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,12 @@ const port = process.env.PORT || 3001
 const app = express()
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	next()
+})
+
 app.use(itemRouter)
 
 app.listen(port, () => {
